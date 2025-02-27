80bb954b90a2d5c684bddb0ae0c91aee75099d55
Kyber <hipermig@gmail.com>
2025-02-27 12:17:21 +0100

    Update userscript_another.sh
    added commentaries and "fixed" ugly spacing

--- userscript_another.sh
+++ userscript_another.sh
@@ -3,11 +3,14 @@
 # Define loop and changing variables
 
 for user in guybrush lechuck; do
+
+	#Get line that contains our user's data
 	data=$(cat /etc/passwd | grep -F $user)
-	#Get login
+	#Get things
 	login=$(echo $data | cut -d ":" -f 1)
 	name=$(echo $data | cut -d ":" -f 5 | cut -d " " -f 1)
 	surname=$(echo $data | cut -d ":" -f 5 | cut -d " " -f 2 | cut -d "," -f 1)
+ 	#Output things
 	echo "* DATA OF USER: $user"
 	echo $login
 	echo $name
